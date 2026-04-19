38594298f4ad7be79ce5ca312e46ed52dd246b28
davguzm20 <davguzm20@gmail.com>
2026-04-19 11:39:41 -0500

    feat: add PokemonFactory

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import pygame
+from pokemon.pokemon_factory import PokemonFactory
 
 BASE_WIDTH, BASE_HEIGHT, SCALE = 320, 240, 2.5
+
 
 def main():
     pygame.init()
     running = True
 
+    pokemons = PokemonFactory.load_all_pokemons("pokemon/pokemones.json")
+    print(f"Fueron cargados {len(pokemons)} pokemons")
+
     pygame.display.set_mode((BASE_WIDTH * SCALE, BASE_HEIGHT * SCALE))
     pygame.display.set_caption("POKEFISIG3")
-    
 
     while running:
         for event in pygame.event.get():
@@ -16,6 +20,7 @@ def main():
                 running = False
 
     pygame.quit()
+
 
 if __name__ == "__main__":
     main()
---
+++ pokemon/pokemon_factory.py
@@ -0,0 +1,55 @@
+import json
+from pokemon.models.pokemon import Pokemon
+from pokemon.enums.pokemon_type import PokemonType
+from pokemon.models.ability import Ability
+from pokemon.models.move import Move
+from pokemon.enums.damage_class import DamageClass
+
+class PokemonFactory:
+    @staticmethod
+    def create_pokemon(data: dict) -> Pokemon:
+        types = [PokemonType(type) for type in data["tipo"]]
+        
+        abilities = [
+            Ability(
+                ability["nombre"], 
+                ability["descripcion"]
+            )
+            for ability in data["habilidades"]
+        ]
+        
+        moves = [
+            Move(
+                accuracy=move["precision"],
+                name=move["nombre"],
+                damage_class=DamageClass(move["clase_daño"]),
+                description=move["descripcion"],
+                power=move["poder"],
+                power_points=move["pp"],
+                type=PokemonType(move["tipo"]),
+                priority=move["prioridad"]
+            )
+            for move in data["movimientos"]
+        ]
+        
+        return Pokemon(
+            name=data["nombre"],
+            types=types,
+            abilities=abilities,
+            moves=moves,
+            hp=data["hp"],
+            attack=data["atk"],
+            defense=data["def"],
+            special_attack=data["spa"],
+            special_defense=data["spd"],
+            speed=data["speed"],
+            weight=data["peso"]
+        )
+    
+    @staticmethod
+    def load_all_pokemons(filepath: str) -> list[Pokemon]:
+        with open(filepath, encoding="utf-8") as file:
+            data = json.load(file)
+            
+        return [PokemonFactory.create_pokemon(pokemon_data) 
+                for pokemon_data in data.values()]

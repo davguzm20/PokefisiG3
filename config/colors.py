a67b8c01a233c8b7c03af4f8ce829f390347df07
Developer <dev@local>
2026-04-21 11:42:00 -0500

    feat: add Button component, Scene and Colors classes

---
+++ config/colors.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+class Colors(Enum):
+    WHITE = (255, 255, 255)
+    BLACK = (0, 0, 0)
+    GOLD = (255, 215, 0)
+    RED = (200, 50, 50)
+    GREEN = (50, 200, 50)
+    BLUE = (50, 50, 200)
+    GRAY = (100, 100, 100)
+    DARK_GRAY = (50, 50, 50)
+    LIGHT_GRAY = (200, 200, 200)

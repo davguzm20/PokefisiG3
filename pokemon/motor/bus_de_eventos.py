85cf0a503084b9d32b01291c45a280913d69d920
Ronald <ronald.munayco@unmsm.edu.pe>
2026-04-21 10:55:37 -0500

    Bus de eventos

---
+++ pokemon/motor/bus_de_eventos.py
@@ -0,0 +1,22 @@
+## Utilizamos el bus de eventos para desacoplar componentes. Es implementación del patrón Observer
+## Aquí se reciben los eventos que se disparan de un componente y se le envía la señal al componente suscrito para realizar la acción que necesite
+
+class EventBus:
+   
+    def __init__(self):
+        self.suscripciones = {}
+
+    # (1) entonces se agrega args y kwarg
+    def disparar(self, suscripcion, *args, **kwargs): #Publicar
+        for dirfuncion in self.suscripciones[suscripcion]:
+            dirfuncion(*args, **kwargs)
+
+    
+    def escuchar(self, nombreSuscripcion, dirfuncion): #Suscribir
+        if nombreSuscripcion not in self.suscripciones:
+            self.suscripciones[nombreSuscripcion] = []
+
+        self.suscripciones[nombreSuscripcion].append(dirfuncion) 
+    #Si solo lee lo que hay en direccion de memoria de la función faltaría definir parametros (1)
+
+bus_de_eventos_global = EventBus() #Importen esta instancia, no la clase

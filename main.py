06ad512016302a72173dd48794c0aee1f39ca440
Developer <dev@local>
2026-04-21 11:43:05 -0500

    update: integrate SceneManager in main

--- main.py
+++ main.py
@@ -1,26 +1,11 @@
-import pygame
 from pokemon.pokemon_factory import PokemonFactory
-
-BASE_WIDTH, BASE_HEIGHT, SCALE = 320, 240, 2.5
-
+from ui.scenes.scene_manager import SceneManager
 
 def main():
-    pygame.init()
-    running = True
-
     pokemons = PokemonFactory.load_all_pokemons("pokemon/pokemones.json")
     print(f"Fueron cargados {len(pokemons)} pokemons")
 
-    pygame.display.set_mode((BASE_WIDTH * SCALE, BASE_HEIGHT * SCALE))
-    pygame.display.set_caption("POKEFISIG3")
-
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-
-    pygame.quit()
-
+    SceneManager.run()
 
 if __name__ == "__main__":
     main()

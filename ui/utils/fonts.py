43ee6b111cc77a38183a12c62265ecd965ab178b
davguzm20 <davguzm20@gmail.com>
2026-05-06 02:16:26 -0500

    feat: add Assets and Fonts utility classes

---
+++ ui/utils/fonts.py
@@ -0,0 +1,11 @@
+import pygame
+
+class Fonts:
+    PIXELIFY_SANS = "config/fonts/PixelifySans-VariableFont_wght.ttf"
+
+    @staticmethod
+    def get_pixelify_sans(size: int) -> pygame.font.Font:
+        try:
+            return pygame.font.Font(Fonts.PIXELIFY_SANS, size)
+        except (FileNotFoundError, pygame.error):
+            return pygame.font.Font(None, size)

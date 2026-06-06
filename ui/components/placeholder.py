0826ccf4b2c957d36231bd2273b7211ae36d3bc0
davguzm20 <davguzm20@gmail.com>
2026-06-06 17:06:59 -0500

    fix: adjust pokemon card size and font

--- ui/components/placeholder.py
+++ ui/components/placeholder.py
@@ -34,6 +34,6 @@ class Placeholder:
         
         if self.label:
             font = Fonts.get_font(self.text_size)
-            text_surface = font.render(self.label, True, text_color)
+            text_surface = font.render(self.label, False, text_color)
             text_rect = text_surface.get_rect(center=rect.center)
             screen.blit(text_surface, text_rect)
